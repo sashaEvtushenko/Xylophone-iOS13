9f43a5150cb540cfea507a1ffbcfe86f7e9ecd38
Sasha Evtushenko <sasha.evtushenko.md@gmail.com>
2020-07-27 21:31:47 +0300

    Completed Challenge with opacity

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        guard let soundName = sender.titleLabel?.text else { return }
+        playSound(soundName: soundName)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
 }
 
